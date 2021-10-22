0d22af1ba90e6334df1e340071b4b129af0a9a4d
Mie Chayakorn <miechayakorn@gmail.com>
2021-10-22 16:59:23 +0700

    [Update] handle error

--- pages/api/calendar/index.js
+++ pages/api/calendar/index.js
@@ -4,11 +4,11 @@ import getEvent from '../../../components/getEvent'
 const Index = async (req, res) => {
 
     if (req.method === 'GET') {
-        let data = await getEvent()
-        if (data != 0) {
+        try {
+            let data = await getEvent()
             res.status(200).json({length: data.length, data: data})
-        } else {
-            res.status(500).json({msg: 'failed'})
+        } catch (e) {
+            res.status(500).json({status: 'failed', error: e})
         }
     } else if (req.method === 'DELETE') {
         let responseGCal = await clearEvent()
