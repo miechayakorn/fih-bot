74c93691e6b6290b09879198d203edd954eabd3c
Mie Chayakorn <miechayakorn@gmail.com>
2021-10-22 16:42:08 +0700

    [Clear] file not use

--- pages/api/calendar/get.js
+++
@@ -1,8 +0,0 @@
-import listCalendar from '../../../components/listCalendar'
-import getEvent from '../../../components/getEvent'
-
-const list = async (req, res) => {
-    res.status(200).json(await getEvent())
-}
-
-export default list
