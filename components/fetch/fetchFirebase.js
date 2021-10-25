f3481301c8d0bf93d1bbc2cb4454c2993ea12e39
Mie Chayakorn <miechayakorn@gmail.com>
2021-10-25 13:01:26 +0700

    [Update] use promise data

--- components/fetch/fetchFirebase.js
+++ components/fetch/fetchFirebase.js
@@ -1,13 +1,11 @@
 import firebase from '../../helper/firebase'
 
 export const fetchFirebase = async (findVar) => {
-    const conn = firebase.ref(findVar)
-
-    let response
-    await conn.on('value', snapshot => {
-        response = snapshot.val()
-    })
-    return response
+    return await (new Promise((resolve) => {
+        firebase.ref(findVar).on('value', snapshot => {
+            resolve(snapshot.val())
+        })
+    }))
 }
 
 export const storeFirebase = async (findVar, data) => {
